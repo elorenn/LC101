8c7118caf96f74dfd480ce218922cd2c64a9740a
Lorena Sarasua <lorena.sarasua@gmail.com>
2021-02-10 20:50:44 -0500

    week 2A strings

---
+++ string_mad_lib.js
@@ -0,0 +1,7 @@
+let pluralNoun = "apples";
+let name = "Pete";
+let verb = "glide";
+let adjective = "neat";
+let color = "periwinkle";
+
+console.log("JavaScript provides a "+ color +" collection of tools — including " + adjective + " syntax and " + pluralNoun + " — that allows "+ name +" to "+ verb +" with strings.");
