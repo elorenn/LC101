36e1b184360581737bb5155ef25c7cf9fbed4472
Lorena Sarasua <lorena.sarasua@gmail.com>
2021-02-11 04:57:17 -0500

    week 2A arrays

---
+++ array_exercises_01.js
@@ -0,0 +1,17 @@
+//Create an array with the following entry: 273.15.
+
+let arr = [273.15];
+console.log(arr);
+
+//Use the push method to add the number 42 and the string "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
+
+arr.push(42);
+console.log(arr);
+
+arr.push("hello");
+console.log(arr);
+
+//Use a single push to add the items false, -4.6, and “87” to the array.  Print the array to confirm the changes.
+
+arr.push(false, -4.6, "87");
+console.log(arr);
---
+++ multidimensional_arrays.js
@@ -0,0 +1,19 @@
+let school = [
+   ["science", "computer", "art"],
+   ["Jones", "Willoughby", "Rhodes"]
+];
+
+//What are the two dimensional indexes for "Jones"?
+
+console.log(school[1][0]);
+
+// How would you add "dance" to the array at school[0]?
+
+school[0].push("dance");
+console.log(school[0]);
+
+// How would you add "Holmes" to the array at school[1]?
+
+school[1].push("Holmes");
+console.log(school[1]);
+
