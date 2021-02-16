d2c801ee9059470ecc9c9c23f009535da7ace7ce
Lorena Sarasua <lorena.sarasua@gmail.com>
2021-02-16 00:48:03 -0500

    Part 2: Multiple Questions, sections 1,2, and 3: ask all five questions, get answers, display results

--- github_classroom/assignment-1-candidate-testing-elorenn/candidate-testing.js
+++ github_classroom/assignment-1-candidate-testing-elorenn/candidate-testing.js
@@ -9,9 +9,9 @@ let candidateName = "";
 let question = 'Who was the first American woman in space? ';
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ['Who was the first American woman in space? ','True or false: 5000 meters = 5 kilometers. ','(5 + 3)/2 * 10 = ? ', `Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? `,'What is the minimum crew size for the ISS? '];
+let correctAnswers = ['Sally Ride','True','40','Trajectory','3'];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -21,17 +21,49 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
+  for (i = 0; i < questions.length ; i++){
+    candidateAnswers[i] = input.question("\n" + questions[i]);
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (correctAnswer.toLowerCase() === candidateAnswer.toLowerCase()) {
-    console.log(`You are correct!`);
-  } else {
-    console.log(`Sorry. ${candidateAnswer} is incorrect. The correct answer is ${correctAnswer}.`);
-  }
+  // if (correctAnswer.toLowerCase() === candidateAnswer.toLowerCase()) {
+  //   console.log(`You are correct!`);
+  // } else {
+  //   console.log(`Sorry. ${candidateAnswer} is incorrect. The correct answer is ${correctAnswer}.`);
+  // }
+  
+  console.log(`
+        * * * RESULTS * * * 
+
+Candidate Name: ${candidateName}
+
+1) ${questions[0]}
+Your Answer: ${candidateAnswers[0]}
+Correct Answer: ${correctAnswers[0]}
+
+2)  ${questions[1]}
+Your Answer:  ${candidateAnswers[1]}
+Correct Answer:  ${correctAnswers[1]}
+
+3) ${questions[2]}
+Your Answer:  ${candidateAnswers[2]}
+Correct Answer:  ${correctAnswers[2]}
+
+4)  ${questions[3]}
+Your Answer:  ${candidateAnswers[3]}
+Correct Answer:  ${correctAnswers[3]}
+
+5)  ${questions[4]}
+Your Answer:  ${candidateAnswers[4]}
+Correct Answer:  ${correctAnswers[4]}
+
+>>> Overall Grade: 40% (2 of 5 responses correct) <<<
+>>> Status: FAILED <<<
+
+  `);
 
   let grade;
   
@@ -42,7 +74,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log(`Hello, ${candidateName}!`);
+  console.log(`\nHello, ${candidateName}!`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
