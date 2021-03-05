bd24958394a6ab3a3cea489ee61b355d44b92bf5
Lorena Sarasua <lorena.sarasua@gmail.com>
2021-03-05 04:44:44 -0500

    refractor launchOutput() to be DRYer

--- Week 05/5A Unit Testing/14.7. Studio Unit Testing/UnitTestingStudio01Expectations/launchCodeRocks.js
+++ Week 05/5A Unit Testing/14.7. Studio Unit Testing/UnitTestingStudio01Expectations/launchCodeRocks.js
@@ -1,57 +1,22 @@
 function launchOutput(num){
-
-  if (num === 2){
-    return 'Launch!';
+  let output = '';
+  if (num%2 === 0  || num%3 === 0 || num%5 === 0) { 
+    if (num%2 === 0){
+      output += 'Launch';
+    }  
+    if (num%3 === 0){
+      output += 'Code';
+    }  
+    if (num%5 === 0){
+      output += ' Rocks';
+    }
+    output = output.trim() + '!'; 
+    if (num%2 === 0 && num%5 === 0 && num%3 !== 0) {
+      output += ' (CRASH!!!!)';
+    } 
+    return output;
   }
-  if (num === 3){
-    return 'Code!';
-  }
-  if (num === 5){
-    return 'Rocks!';
-  }
-  if (num%2 === 0  && num%3 === 0 && num%5 === 0) {
-    return 'LaunchCode Rocks!';
-  }     
-  if (num%2 === 0 && num%3 === 0) {
-    return 'LaunchCode!';
-  } 
-  if (num%3 === 0 && num%5 === 0) {
-    return 'Code Rocks!';
-  }  
-  if (num%2 === 0 && num%5 === 0) {
-    return 'Launch Rocks! (CRASH!!!!)';
-  }  
-
-  return `Rutabagas! That doesn't work.`;
+  return `Rutabagas! That doesn't work.`; 
 }
-
-// function launchOutput(num){
-  
-//   let output = '';
-
-//   if (num === 2){
-//     output += 'Launch';
-//   }
-//   if (num === 3){
-//     output += 'Code';
-//   }
-//   if (num === 5){
-//     output += 'Rocks';
-//   }
-//   if (num%2 === 0 && num%3 === 0) {
-//     output += 'LaunchCode';
-//   }
-//   if (num%3 === 0 && num%5 === 0) {
-//     output += 'Code Rocks';
-//   }  
-//   if (num%2 === 0 && num%5 === 0) {
-//     output += 'Launch Rocks';
-//   }  
-//   if (num%2 === 0  && num%3 === 0 && num%5 === 0) {
-//     output += 'Launch Rocks';
-//   }  
-
-//   return output + '!';
-// }
 
 module.exports = launchOutput;
